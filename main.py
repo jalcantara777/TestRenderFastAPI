dc2ce771db07b597296d01bb2871fcf2ed202d78
José Alcántara <jalcantara777@yahoo.com>
2024-03-14 20:17:26 -0500

    last chgs: new function names

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def message():
     return "Jehová Jireh"
 
 @app.get('/sucursales_id')
-def get_sucursal_address_1(sucursal_id:int):
-    linea1 = 'Sucursal : '+str.upper(sucursales.iloc[sucursal_id,1])
-    linea2 = 'Dirección: '+sucursales.iloc[sucursal_id,2]
+def get_sucursal_address_from_integer(sucursal_id:int= Query(None, description="Ingrese un valor entero entre el rango 1-27")):
+    linea1 = 'Sucursal : '+str.upper(sucursales.iloc[sucursal_id-1,1])
+    linea2 = 'Dirección: '+sucursales.iloc[sucursal_id-1,2]
     valret = f"{linea1}    {linea2}"
     return valret
 
 @app.get('/sucursales_name')
-def get_sucursal_address_2(sucursal_name: str= Query(None, description=f"Ingrese desde las 3 primeras letras de los valores posibles de esta lista: {lst_suc_names}")):
+def get_sucursal_address_from_string(sucursal_name: str= Query(None, description=f"Ingrese desde las 3 primeras letras de los valores posibles de esta lista: {lst_suc_names}")):
     sucursal_id=sucursales[sucursales.Sucursal.str.lower().str.startswith(sucursal_name.lower())].ID.iloc[0]
     sucursal_nm=sucursales[sucursales.Sucursal.str.lower().str.startswith(sucursal_name.lower())].Sucursal.iloc[0]
     sucursal_ad=sucursales[sucursales.Sucursal.str.lower().str.startswith(sucursal_name.lower())].Direccion.iloc[0]
